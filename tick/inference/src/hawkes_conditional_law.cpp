5097c48bd3a7d833d0e399912a26d156a7313eb2
Emmanuel Bacry <emmanuel.bacry@polytechnique.fr>
2017-07-05 10:01:15 +0200

    TICK-381 Minor changes
    TICK-381
    Removed y_mark argument from PointProcessCondLaw
    
    TICK-381
    Cleaning cpp code
    
    TICK-381
    return count
    
    TICK-381
    Update
    
    TICK-381
    Still a bug
    
    TICK-381
    Done !
    
    TICK-381 fixed update of Lambda
    
    TICK-381 fix cpplint style errors
    
    TICK-381 changed default value for T to None
    
    TICK-381 some fixes

--- tick/inference/src/hawkes_conditional_law.cpp
+++ tick/inference/src/hawkes_conditional_law.cpp
@@ -14,97 +14,127 @@
 
 // The non parametric estimation is based on the following quantities :
 //
-// Given two point processes Y and Z, computes the Signal
+// Given two point processes Y and Z, computes (for all i) the Signal
 //
-//      E((Y[t+lag+delta+epsilon]-Y[t+lag+epsilon]) | zmin<=Z[t]<=zmax) - Lambda_Y
+//      E((Y[t+lags[i+1]+epsilon]-Y[t+lags[i]+epsilon]) | zmin<=Z[t]<=zmax) - Lambda_Y
 //
 // where epsilon is infinitely small
 //
-void PointProcessCondLaw(ArrayDouble &y_time, ArrayDouble &y_mark,
+//  - lags : array of lags to loop on. It is of size N+1, so it represents N intervals.
+//  - y_T : end time of Y signal
+//  - y_lambda : unconditionnal intensity of Y
+//
+// The result is stored in res_X (abscissa) and res_Y (ordinates) : they are of size N
+//
+// Returns the number of terms involved in computation.
+//
+
+// No mark for y !!
+
+void PointProcessCondLaw(ArrayDouble &y_time,
                          ArrayDouble &z_time, ArrayDouble &z_mark,
                          ArrayDouble &lags,
                          double zmin, double zmax,
+                         double y_T,
+                         double y_lambda,
                          ArrayDouble &res_X, ArrayDouble &res_Y) {
-  ulong y_index_lag;
-  ulong y_index_lag_delta;
-  double ytlag;
-  double ytlagdelta;
-  double lag;
-  ulong N = lags.size() - 1;
-
-  ArrayDouble end_slice = ArrayDouble(N);
-  ArrayULong tab_y_index = ArrayULong(N);
-
-  Py_BEGIN_ALLOW_THREADS
-
-  for (ulong i = 0; i < res_Y.size(); i++) {
-    res_Y[i] = 0;
-    res_X[i] = lags[i];
-  }
-
-  double count = 0;
-
-  // The end of slices
-  for (ulong i = 0; i < N; i++) end_slice[i] = lags[i + 1];
-
-  double lagMax = end_slice[N - 1];
-
-  // To improve performance by remembering the last point of Y in each time slice
-  tab_y_index.fill(0);
-
-  // Average intensity of y
-  double lambda = (y_mark[y_mark.size() - 1] - y_mark[0]) /
-      (y_time[y_mark.size() - 1] - y_time[0]);
-
-  // The loop on the jumps of Z
-  ulong y_index = 0;
-  for (ulong z_index = 0; z_index < z_mark.size(); z_index++) {
-    // Is it an eligible jump ?
-    if (zmin < zmax && z_index > 0 &&
-        (zmin > z_mark[z_index] - z_mark[z_index - 1]
-            || z_mark[z_index] - z_mark[z_index - 1] > zmax))
-      continue;
-
-
-    // Brings y_index after z_t
-    // After this block, one has
-    // time(Y[y_index]) >= z_t
-    double z_t = z_time[z_index];
-    if (z_t + lagMax >= y_time[y_time.size() - 1]) break;
-    while (y_index < y_time.size() && y_time[y_index] < z_t) y_index++;
-    if (y_index >= y_time.size()) break;
-    count += 1;
-
-    y_index_lag_delta = y_index;
-
-    // Loop on the lag
-    for (ulong k = 0; k < N; k++) {
-      lag = res_X[k];
-      y_index_lag = y_index_lag_delta;
-
-      while (y_time[y_index_lag] <= z_t + lag) {
-        y_index_lag++;
-      }
-      ytlag = (y_index_lag == 0 ? 0 : y_mark[y_index_lag - 1]);
-
-      y_index_lag_delta = std::max(y_index_lag, tab_y_index[k]);
-      while (y_time[y_index_lag_delta] <= z_t + end_slice[k]) {
-        y_index_lag_delta++;
-      }
-      tab_y_index[k] = y_index_lag_delta;
-      ytlagdelta = (y_index_lag_delta == 0 ? 0 : y_mark[y_index_lag_delta - 1]);
-      res_Y[k] += ytlagdelta - ytlag;
+    if (z_time.size() != z_mark.size()) {
+        TICK_ERROR("z_time (size=" << z_time.size() << ") and z_mark (size=" << z_mark.size()<< ") should have the same size");
     }
-  }
 
-  for (ulong k = 0; k < N; k++) {
-    res_Y[k] /= count;
-    res_Y[k] /= (end_slice[k] - res_X[k]);
-    res_Y[k] -= lambda;
-    res_X[k] = (end_slice[k] + res_X[k]) / 2.0;
-  }
+    if (res_X.size() != res_Y.size()) {
+        TICK_ERROR("res_X (size=" << res_X.size() << ") and res_Y (size=" << res_Y.size()<< ") should have the same size");
+    }
 
-  Py_END_ALLOW_THREADS
+    if (res_X.size() != lags.size()-1) {
+        TICK_ERROR("lags (size=" << lags.size() << ") should be of the size of res_X (size=" << res_X.size()<< " plus one");
+    }
+
+    ulong y_index_lag;
+    ulong y_index_lag_delta;
+    double ytlag;
+    double ytlagdelta;
+    double lag;
+    ulong N = lags.size() - 1;
+
+    ArrayULong tab_y_index = ArrayULong(N);
+
+    ulong n_terms = 0;
+
+    Py_BEGIN_ALLOW_THREADS
+
+    // Inititialization of the resulting array
+    res_Y.fill(0);
+
+    double lagMax = lags[N];
+
+    // To improve performance by remembering the last point of Y in each time slice
+    tab_y_index.fill(0);
+
+    // The loop on the jumps of Z
+    ulong y_index = 0;
+    for (ulong z_index = 0; z_index < z_mark.size(); z_index++) {
+        // Is it an eligible jump ?
+        if (zmin < zmax && z_index > 0 &&
+            (zmin > z_mark[z_index] - z_mark[z_index - 1]
+             || z_mark[z_index] - z_mark[z_index - 1] > zmax))
+            continue;
+
+        // Brings y_index after z_t
+        // After this block, one has
+        // time(Y[y_index]) >= z_t
+        double z_t = z_time[z_index];
+        if (z_t + lagMax >= y_T) break;
+        n_terms += 1;
+        while (y_index < y_time.size() && y_time[y_index] < z_t) y_index++;
+        if (y_index >= y_time.size()) break;
+
+        y_index_lag_delta = y_index;
+
+        // Loop on the lag
+        for (ulong k = 0; k < N; k++) {
+            lag = lags[k];
+            y_index_lag = y_index_lag_delta;
+
+            while (y_time[y_index_lag] <= z_t + lag) {
+                y_index_lag++;
+                if (y_index_lag >= y_time.size()) break;
+            }
+            if (y_index_lag >= y_time.size()) {
+                y_index_lag_delta = y_time.size()-1;
+                tab_y_index[k] = y_index_lag_delta;
+                continue;
+            }
+            ytlag = (y_index_lag == 0 ? 0 : y_index_lag - 1);
+
+            y_index_lag_delta = std::max(y_index_lag, tab_y_index[k]);
+            while (y_time[y_index_lag_delta] <= z_t + lags[k+1]) {
+                y_index_lag_delta++;
+                if (y_index_lag_delta >= y_time.size()) break;
+            }
+            if (y_index_lag_delta >= y_time.size()) {
+                y_index_lag_delta = y_time.size()-1;
+                if (y_index_lag == y_time.size()-1) {
+                    tab_y_index[k] = y_index_lag_delta;
+                    continue;
+                }
+            }
+            tab_y_index[k] = y_index_lag_delta;
+            ytlagdelta = (y_index_lag_delta == 0 ? 0 : y_index_lag_delta - 1);
+            res_Y[k] += ytlagdelta - ytlag;
+        }
+    }
+
+    for (ulong k = 0; k < N; k++) {
+        if (n_terms != 0) {
+            res_Y[k] /= n_terms;
+        }
+        res_Y[k] /= (lags[k+1] - lags[k]);
+        res_Y[k] -= y_lambda;
+        res_X[k] = (lags[k+1] + lags[k]) / 2.0;
+    }
+
+    Py_END_ALLOW_THREADS
 }
 
 //
@@ -121,59 +151,59 @@ double PointProcessCondLawSingle(ArrayDouble &y_time,
                                  double delta,
                                  double zmin,
                                  double zmax) {
-  double res = 0;
-  Py_BEGIN_ALLOW_THREADS
+    double res = 0;
+    Py_BEGIN_ALLOW_THREADS
 
-  const double lambda = y_time.size() / (y_time[y_time.size() - 1] - y_time[0]);
+    const double lambda = y_time.size() / (y_time[y_time.size() - 1] - y_time[0]);
 
-  // The loop on the jumps of Z
-  std::int64_t count = 0;
-  ulong y_index = 0;
-  ulong y_index_delta = 0;
-  for (ulong z_index = 0; z_index < z_time.size(); z_index++) {
-    // Is it an eligible jump ?
-    if (zmin < zmax && z_index > 0 &&
-        (zmin > z_mark[z_index] - z_mark[z_index - 1]
-            || z_mark[z_index] - z_mark[z_index - 1] > zmax))
-      continue;
+    // The loop on the jumps of Z
+    std::int64_t count = 0;
+    ulong y_index = 0;
+    ulong y_index_delta = 0;
+    for (ulong z_index = 0; z_index < z_time.size(); z_index++) {
+        // Is it an eligible jump ?
+        if (zmin < zmax && z_index > 0 &&
+            (zmin > z_mark[z_index] - z_mark[z_index - 1]
+             || z_mark[z_index] - z_mark[z_index - 1] > zmax))
+            continue;
 
-    double z_t = z_time[z_index];
+        double z_t = z_time[z_index];
 
-    // Looking for index of y[t]
-    while (y_index < y_mark.size() && y_time[y_index] < z_t) y_index++;
+        // Looking for index of y[t]
+        while (y_index < y_mark.size() && y_time[y_index] < z_t) y_index++;
 
-    if (y_index >= y_time.size())
-      break;
+        if (y_index >= y_time.size())
+            break;
 
-    double yt;
+        double yt;
 
-    if (y_mark[y_index] == z_t)
-      yt = y_mark[y_index];
-    else
-      yt = (y_index == 0 ? 0 : y_mark[y_index - 1]);
+        if (y_mark[y_index] == z_t)
+            yt = y_mark[y_index];
+        else
+            yt = (y_index == 0 ? 0 : y_mark[y_index - 1]);
 
-    if (z_t + delta >= y_mark[y_mark.size() - 1]) break;
+        if (z_t + delta >= y_mark[y_mark.size() - 1]) break;
 
-    count += 1;
+        count += 1;
 
-    // Looking for index for y[t+delta]
-    y_index_delta = std::max(y_index_delta, y_index);
-    while (y_mark[y_index_delta] <= z_t + delta) y_index_delta++;
+        // Looking for index for y[t+delta]
+        y_index_delta = std::max(y_index_delta, y_index);
+        while (y_mark[y_index_delta] <= z_t + delta) y_index_delta++;
 
-    double ytdelta = 0;
+        double ytdelta = 0;
 
-    if (y_mark[y_index_delta] == z_t + delta)
-      ytdelta = y_mark[y_index_delta];
-    else
-      ytdelta = (y_index_delta == 0 ? 0 : y_mark[y_index_delta - 1]);
+        if (y_mark[y_index_delta] == z_t + delta)
+            ytdelta = y_mark[y_index_delta];
+        else
+            ytdelta = (y_index_delta == 0 ? 0 : y_mark[y_index_delta - 1]);
 
-    res += ytdelta - yt - lambda * delta;
-  }
+        res += ytdelta - yt - lambda * delta;
+    }
 
-  res /= count;
+    res /= count;
 
-  Py_END_ALLOW_THREADS
+    Py_END_ALLOW_THREADS
 
-  return res;
+    return res;
 }
 
--- tick/inference/tests/hawkes_conditional_law_test.py
+++ tick/inference/tests/hawkes_conditional_law_test.py
@@ -21,8 +21,8 @@ class Test(unittest.TestCase):
         """...Test HawkesConditionalLaw kernels norm estimation
         """
         np.testing.assert_array_almost_equal(self.model.kernels_norms,
-                                             [[-0.7813225, -1.12426198],
-                                              [-1.19782933, -1.75748751]])
+                                             [[-0.81130911, -1.12992177],
+                                              [-1.16313257, -1.72348019]])
 
     def test_hawkes_conditional_law_kernels(self):
         """...Test HawkesConditionalLaw kernel estimation
@@ -33,16 +33,16 @@ class Test(unittest.TestCase):
         np.testing.assert_array_almost_equal(self.model.kernels, saved_phi)
 
     def test_hawkes_conditional_law_baseline(self):
-        """...Test HawkesConditionalLaw basleine estimation
+        """...Test HawkesConditionalLaw baseline estimation
         """
         np.testing.assert_array_almost_equal(self.model.baseline,
-                                             [0.616481, 0.831181])
+                                             [0.61213243458370525, 0.80888642567386082])
 
     def test_hawkes_conditional_mean_intensity(self):
         """...Test HawkesConditionalLaw mean intensity estimation
         """
         np.testing.assert_array_almost_equal(self.model.mean_intensity,
-                                             [0.21470093, 0.20816257])
+                                             [0.2081211779170711, 0.2081211779170711])
 
     def test_hawkes_quad_method(self):
         """...Test HawkesConditionalLaw estimates with different quadrature
@@ -51,26 +51,26 @@ class Test(unittest.TestCase):
         model = HawkesConditionalLaw(n_quad=5, quad_method='gauss')
         model.fit(self.timestamps)
         np.testing.assert_array_almost_equal(model.kernels_norms,
-                                             [[-0.7813225, -1.12426198],
-                                              [-1.19782933, -1.75748751]])
+                                             [[-0.81130911, -1.12992177],
+                                              [-1.16313257, -1.72348019]])
 
         model = HawkesConditionalLaw(n_quad=5, quad_method='gauss-')
         model.fit(self.timestamps)
         np.testing.assert_array_almost_equal(model.kernels_norms,
-                                             [[-224.0366795, 36.68554746],
-                                              [-174.78323768, 33.31661676]])
+                                             [[-77.76904711, 0.69985519],
+                                              [-42.87140913 , 0.13607425]])
 
         model = HawkesConditionalLaw(n_quad=5, quad_method='lin')
         model.fit(self.timestamps)
         np.testing.assert_array_almost_equal(model.kernels_norms,
-                                             [[15.09698511, 1.94961027],
-                                              [-30.99208312, 14.75358309]])
+                                             [[7.92561315, 1.74540188],
+                                              [-28.57048537, 10.77926367]])
 
         model = HawkesConditionalLaw(n_quad=5, quad_method='log')
         model.fit(self.timestamps)
         np.testing.assert_array_almost_equal(model.kernels_norms,
-                                             [[-1.5177032, -4.56946934],
-                                              [2.09935106, 3.36845494]])
+                                             [[35.70738975, 18.96902121],
+                                              [-51.69638233, -30.33936597]])
 
     def test_hawkes_claw_method(self):
         """...Test HawkesConditionalLaw estimates with different conditional
@@ -79,15 +79,19 @@ class Test(unittest.TestCase):
         model = HawkesConditionalLaw(n_quad=5, claw_method='lin')
         model.incremental_fit(self.timestamps, compute=False)
         model.compute()
+        model.fit(self.timestamps)
         np.testing.assert_array_almost_equal(model.kernels_norms,
-                                             [[-0.7813225, -1.12426198],
-                                              [-1.19782933, -1.75748751]])
+                                             [[-0.81130911, -1.12992177],
+                                              [-1.16313257, -1.72348019]])
 
         model = HawkesConditionalLaw(n_quad=5, claw_method='log')
         model.incremental_fit(self.timestamps)
+        model.compute()
+        print(model.kernels_norms)
+
         np.testing.assert_array_almost_equal(model.kernels_norms,
-                                             [[0.42528942, -0.49200346],
-                                              [-1.18794187, -6.19311372]])
+                                             [[0.46108403, -0.09467477],
+                                              [-0.04787463, -3.82917571]])
 
 if __name__ == "__main__":
     unittest.main()
